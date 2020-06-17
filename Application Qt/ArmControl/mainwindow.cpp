03f99bdc6300591f6716fcba5a339e42af6de96c
AdrienCMS <adrien.salle@aol.com>
2020-06-17 15:00:52 +0200

    17/06/2020

--- Application Qt/ArmControl/mainwindow.cpp
+++ Application Qt/ArmControl/mainwindow.cpp
@@ -3,12 +3,18 @@
 
 #include <QSerialPort>
 #include <QSerialPortInfo>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("C:/Users/Adrien/Documents/Projet Bras robot/LP-MECSE2020-Projet-Bras-Robot/Application Qt/ArmControl/Pictures/hand_.png");
+    int w = ui->label_hand->width();
+    int h = ui->label_hand->height();
+
+    ui->label_hand->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatioByExpanding));
 }
 
 MainWindow::~MainWindow()
